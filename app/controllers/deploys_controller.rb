642a063bcb1762d40cac3116820fbaa27efc6ba5
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-09-26 16:07:12 +0200

    Create deploy event with specific environment

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -1,11 +1,15 @@
 class DeploysController < BaseController
   def create
+    environment = Environment.find_by_id(params["data"]["id"])
+
     deploy_event = DeployEvent.create!(
       site: Site.first,
       event_type: DeployEvent::REQUEST,
-      environment: Rails.env,
+      environment: environment,
       data: {}
     )
+
+    environment.update_attribute(:deploy_status, "pending")
 
     DeployJob.perform_later(deploy_event.id)
   end
