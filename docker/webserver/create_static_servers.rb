395ad05c3b0f75578da28c68a6a684040256b697
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-10-05 10:16:53 +0200

    Serve static domains

--- docker/webserver/create_static_servers.rb
+++ docker/webserver/create_static_servers.rb
@@ -14,15 +14,13 @@ end
 
 static_domains = ENV["STATIC_DOMAINS"] || []
 parsed_static_domains = load_static_domains(static_domains)
-template = File.read("./static_site.conf")
+template = File.read("./static_servers.conf")
 
-blocks = begin
-  parsed_static_domains.map do |domain|
-    result = template.gsub("${STATIC_SITE_DOMAIN}", domain["domain"])
-  end
+blocks = parsed_static_domains.map do |domain|
+  template.gsub("${STATIC_SITE_DOMAIN}", domain["domain"])
 end
 
 File.write(
-  "/usr/local/openresty/nginx/conf/sites-enabled/https_app.conf",
+  "/usr/local/openresty/nginx/conf/sites-enabled/https_static_servers.conf",
   blocks.join("\n\n")
 )
