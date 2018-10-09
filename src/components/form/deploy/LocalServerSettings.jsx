5ae7a0e0fc368cb45f46895de770c1de8685aee4
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-10-09 11:02:31 +0200

    Improve local server settings for deployment

--- src/components/form/deploy/LocalServerSettings.jsx
+++ src/components/form/deploy/LocalServerSettings.jsx
@@ -1,12 +1,34 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Component from 'components/BaseComponent';
+import { SubmitButton, Form } from 'components/form'
+import { reduxForm } from 'redux-form'
+import Instructions from 'components/sub/Instructions'
 
 class LocalServerSettings extends Component {
   render() {
+    const {
+      error,
+      submitting,
+      handleSubmit,
+      valid
+    } = this.props
+
+    const dirty = true
+
     return (
       <div className="LocalServerSettings">
-        Ciao
+        <Form error={error} onSubmit={handleSubmit}>
+          <div className="Page__content--note">
+            <Instructions value={this.t('environment.deployAdapters.local_server.instruction')} />
+          </div>
+          <SubmitButton
+            submitting={submitting}
+            dirty={dirty}
+            valid={valid}
+            intlLabel="environment.deployAdapters.sftp.button"
+          />
+        </Form>
       </div>
     )
   }
@@ -14,9 +36,27 @@ class LocalServerSettings extends Component {
 
 LocalServerSettings.smallModal = true
 
-LocalServerSettings.proptypes = {
+const formConfig = {
+  form: 'LocalServerSettings'
+}
+
+LocalServerSettings.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  dirty: PropTypes.bool.isRequired,
+  valid: PropTypes.bool.isRequired,
+  error: PropTypes.string,
   settings: PropTypes.object,
   dispatch: PropTypes.func.isRequired,
 }
 
-export default connect()(LocalServerSettings)
+function mapStateToProps(state, props) {
+  return {
+    onSubmit() {
+      return props.onSubmit({
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps)(reduxForm(formConfig)(LocalServerSettings))
