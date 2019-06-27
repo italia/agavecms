2e9fc730726d86e7cf37f0a057427bf8348dbc62
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-06-27 12:03:59 +0200

    Drop fill form login helpers and add command to set authenticated session

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,6 @@
-Cypress.Commands.add('loginByForm', (email, password) => {
-  cy.get('input[name=email]').type(email)
-  cy.get('input[name=password]').type(password)
-
-  return cy.get('form').submit()
-})
-
-Cypress.Commands.add('clearLoginForm', () => {
-  cy.get('input[name=email]').clear()
-  cy.get('input[name=email]').clear()
+Cypress.Commands.add('setSession', () => {
+  window.localStorage.setItem(
+    'persistedState',
+    '{"session" :{"bearerToken": "rwtoken"}}'
+  )
 })
