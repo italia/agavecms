3c27747b603cade63c56e9d2d42416769bc8fc74
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-06-27 11:27:28 +0200

    Improve home page spec

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -3,6 +3,43 @@ describe('The Home Page for non-autorized users', () => {
     cy.visit('/')
     cy.location('pathname').should('eq', '/sign_in')
   })
+
+  context('With wrong credentials', () => {
+    beforeEach(() => {
+      cy.visit('/sign_in')
+      cy.loginByForm('foo@bar.com', 'foo_bar')
+    })
+
+    afterEach(() => {
+      cy.clearLoginForm()
+    })
+
+    it('Displays form error', () => {
+      cy.get('div.form__global-error').should('be.visible')
+    })
+
+    it('Displays modal error', () => {
+      cy.get('div.Notifications__item__message').should('be.visible')
+    })
+
+    it('Stays on the same URL', () => {
+      cy.url().should('include', '/sign_in')
+    })
+  })
+
+  context('When user is logged', () =>{
+    const user = "admin@agave.example.it"
+    const password = "secret"
+
+    beforeEach(() => {
+      cy.visit('/sign_in')
+      cy.loginByForm(user, password)
+    })
+
+    it('Redirects to /admin/item_types', () => {
+      cy.url().should('include', '/admin/item_types')
+    })
+  })
 })
 
 describe('The Home Page for autorized users', () => {
