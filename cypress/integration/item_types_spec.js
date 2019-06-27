a36ffb76381d1b2a912763ae1f85481afefcf4a7
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-06-27 16:26:22 +0200

    Create item types spec

---
+++ cypress/integration/item_types_spec.js
@@ -0,0 +1,85 @@
+describe('The ItemTypes', () => {
+  it('requires login', () => {
+    cy.visit('/admin/item_types')
+    cy.location('pathname').should('eq', '/sign_in')
+  })
+
+  describe('When user is logged', () => {
+    beforeEach(() => {
+      cy.setSession()
+    })
+
+    afterEach(() => {
+      cy.clearSession()
+    })
+
+    context('With an item type', () => {
+      let new_field_id = ""
+      let new_item_type_id = ""
+
+      beforeEach(() => {
+        cy.request({
+          method: 'POST',
+          url: 'http://agave.lvh.me:3000/api/item-types',
+          headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer rwtoken'
+          },
+          body: {
+            "data": {
+              "type": "item_type",
+              "attributes": {
+                "name": "Blog post",
+                "api_key": "post",
+                "singleton": false,
+                "sortable": false,
+                "tree": true,
+                "ordering_direction": null
+              },
+              "relationships": {
+                "ordering_field": {
+                  "data": null
+                }
+              }
+            }
+          }
+        }).then((response) => {
+          new_item_type_id = response.body.data.id
+
+          cy.request({
+            method: 'POST',
+            url: `http://agave.lvh.me:3000/api/item-types/${new_item_type_id}/fields`,
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json',
+              'Authorization': 'Bearer rwtoken'
+            },
+            body: {
+              "data": {
+                "type": "field",
+                "attributes": {
+                  "label": "Price",
+                  "api_key": "price",
+                  "hint": "",
+                  "field_type": "float",
+                  "validators": {},
+                  "localized": false,
+                  "position": 1,
+                  "appeareance": {}
+                }
+              }
+            }
+          })
+        })
+      })
+
+      it('Contains price field', () => {
+        cy.visit(`/admin/item_types/`)
+
+        cy.get('.ItemTypeRow').click()
+        cy.contains("Price")
+      })
+    })
+  })
+})
