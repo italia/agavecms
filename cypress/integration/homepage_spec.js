ab86177a7decae59a31889d72e82beeb999605c6
Joe Yates <joe.g.yates@gmail.com>
2019-06-27 11:21:52 +0200

    Add test for logged-in users

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,6 +1,20 @@
-describe('The Home Page', () => {
+describe('The Home Page for non-autorized users', () => {
   it('requires login', () => {
     cy.visit('/')
     cy.location('pathname').should('eq', '/sign_in')
+  })
+})
+
+describe('The Home Page for autorized users', () => {
+  beforeEach(() => {
+    window.localStorage.setItem(
+      'persistedState',
+      '{"session" :{"bearerToken": "rwtoken"}}'
+    )
+  })
+
+  it('redirects users to setup', () => {
+    cy.visit('/')
+    cy.location('pathname').should('eq', '/admin/item_types')
   })
 })
