ede8acfb6e7a8c56e6e928ca4168cacb6b91e004
Joe Yates <joe.g.yates@gmail.com>
2019-07-01 17:06:36 +0200

    Only allow test environment

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,11 +1,10 @@
 require "seeds"
 
 class TestController < BaseController
+  before_action :permit_only_test_environment
   skip_before_action :ensure_current_site!, only: :reset
 
   def reset
-    raise "RAILS_ENV is not test" if !Rails.env.test?
-
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean
     Seeds.new.setup
@@ -15,5 +14,11 @@ class TestController < BaseController
     session = UserSession.new(User.first, Site.first)
 
     render json: {data: {token: session.access_token}}
+  end
+
+  private
+
+  def permit_only_test_environment
+    raise "RAILS_ENV is not test" if !Rails.env.test?
   end
 end
